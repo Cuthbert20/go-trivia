66cf3ec795131e293b0d369e5a0b3b922a3e4328
Spencer Knowlton <knowltonspencer@gmail.com>
2022-12-10 14:55:25 -0700

    updating home to respond with curr facts

--- handlers/facts.go
+++ handlers/facts.go
@@ -1,7 +1,31 @@
 package handlers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/Cuthbert20/go-trivia/database"
+	"github.com/Cuthbert20/go-trivia/models"
+	"github.com/gofiber/fiber/v2"
+)
 
-func Home(c *fiber.Ctx) error {
-	return c.SendString("Helloo,  Stu lets have some fun with Trivia!!! test")
+// Get Home handler
+func ListFacts(c *fiber.Ctx) error {
+	facts := []models.Fact{}
+
+	database.DB.Db.Find(&facts)
+
+	return c.Status(200).JSON(facts)
+}
+
+// Post Fact Handler
+func CreateFact(c *fiber.Ctx) error {
+	fact := new(models.Fact)
+	//error handling
+	if err := c.BodyParser(fact); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+
+	database.DB.Db.Create(&fact)
+
+	return c.Status(200).JSON(fact)
 }
