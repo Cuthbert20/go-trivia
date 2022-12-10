c8d6004025d50fbab8c777f18da07529e551a205
Spencer Knowlton <knowltonspencer@gmail.com>
2022-12-10 08:13:10 -0700

    updating main

--- cmd/routes.go
+++ cmd/routes.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"github.com/Cuthbert20/go-trivia/handlers"
+	"github.com/gofiber/fiber/v2"
 )
 
-func setupRoutes(app *filber.App) {
+func setupRoutes(app *fiber.App) {
 
 	app.Get("/", handlers.Home)
 
